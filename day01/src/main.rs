54b3a9822363397b7051c1957c1b0172a12c643f
Severen Redwood <severen@shrike.me>
2018-12-04 01:55:44 +1300

    AoC (Day 1): Read input from file instead of stdin

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    io::{self, Read},
+    fs::File,
+    io::Read,
     collections::HashSet,
 };
 
@@ -7,7 +8,7 @@ type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
 fn main() -> Result<()> {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
+    File::open("input.txt")?.read_to_string(&mut input)?;
 
     part1(&input)?;
     part2(&input)?;
