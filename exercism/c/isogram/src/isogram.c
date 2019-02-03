e7ba86b32ec5565c71a082a24e4d455b2477f881
Severen Redwood <severen@shrike.me>
2019-02-03 15:28:50 +1300

    Exercism (C): Improve code style for "Isogram"

--- exercism/c/isogram/src/isogram.c
+++ exercism/c/isogram/src/isogram.c
@@ -9,7 +9,9 @@ bool is_isogram(const char word[]) {
   for (size_t i = 0; word[i] != '\0'; i++) {
     char c = word[i];
 
-    if (!isalpha(c)) continue;
+    if (!isalpha(c)) {
+      continue;
+    }
 
     int i = tolower(c) - 'a';
 
