ea2f1b5faa285c9ad9ab406f3a42ee5f1e23ad7b
Severen Redwood <severen@shrike.me>
2019-02-14 10:23:39 +1300

    AoC (Day 1): Add a solution in Haskell

--- advent-of-code/2018/README.md
+++ advent-of-code/2018/README.md
@@ -4,12 +4,21 @@ These are my solutions for the [Advent of Code
 2018](https://adventofcode.com/2018).
 
 The language I have chosen to implement my solutions in this year is
-[Rust](https://rust-lang.org/).
+[Rust](https://rust-lang.org/), with an additional attempt to write the
+solutions in [Haskell](https://haskell.org/) as well.
 
-To run a solution, enter its directory and invoke the program with Cargo. For
-example:
+To run a Rust solution, enter its directory and invoke the program with
+Cargo. For example:
 
 ```sh
-$ cd day01
+$ cd rust/day01
 $ cargo run
+```
+
+To run a Haskell solution, enter its directory and invoke the program with
+`runhaskell`. For example:
+
+```sh
+$ cd haskell
+$ runhaskell day01.hs
 ```
---
+++ advent-of-code/2018/haskell/day01.hs
@@ -0,0 +1,60 @@
+module Main (main) where
+
+import System.IO
+import System.IO.Error (isDoesNotExistError)
+import Control.Monad
+import Control.Exception
+import qualified Data.Set as Set
+
+-- | Read a file as a string.
+--
+-- This function is a safe(r) wrapper around `readFile` that will return
+-- `Nothing` on `isDoesNotExistError` and propagate any other exceptions.
+readFile' :: String -> IO (Maybe String)
+readFile' p = fmap Just (readFile p) `catch` handleError
+  where
+    handleError :: IOException -> IO (Maybe String)
+    handleError e
+      | isDoesNotExistError e = return Nothing
+      | otherwise = throwIO e
+
+-- | Read an integer.
+--
+-- This function is a specialisation of `read` to `Integer` that also handles a
+-- leading '+'.
+readInt :: String -> Integer
+readInt ('+':xs) = read xs
+readInt xs = read xs
+
+-- | Parse input that is a string of lines of numbers into a list of integers.
+parseInput :: String -> [Integer]
+parseInput = map readInt . lines
+
+part1 :: String -> Integer
+part1 = sum . parseInput
+
+-- | Find the element that is repeated first in a given list.
+firstRepeated :: Ord a => [a] -> Maybe a
+firstRepeated = go Set.empty
+  where
+    go seen (x:xs)
+      | x `Set.member` seen = Just x
+      | otherwise = go (x `Set.insert` seen) xs
+    go _ [] = Nothing
+
+part2 :: String -> Maybe Integer
+part2 = firstRepeated . scanl (+) 0 . cycle . parseInput
+
+run :: String -> IO ()
+run input = do
+  putStrLn $ "Part 1: " ++ show (part1 input)
+  case part2 input of
+    Just answer -> putStrLn $ "Part 2: " ++ show answer
+    Nothing -> putStrLn "Part 2: Answer not found."
+
+main :: IO ()
+main = do
+  input <- readFile' "input.txt"
+  case input of
+    Just contents -> run contents
+    Nothing -> putStrLn "Could not find input file."
