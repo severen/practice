e6b976a0a7ea6ceda094584d08e4e4a7c9bd24bc
Severen Redwood <severen@shrike.me>
2019-02-15 12:08:50 +1300

    Exercism (Haskell): Use rem over div for efficiency in "Leap"
    This commit changes the Haskell solution for "Leap" to use rem instead of mod,
    since it is faster when testing for divisibility.

--- exercism/haskell/leap/src/LeapYear.hs
+++ exercism/haskell/leap/src/LeapYear.hs
@@ -1,4 +1,4 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = year `mod` 4 == 0 && year `mod` 100 /= 0 || year `mod` 400 == 0
+isLeapYear year = year `rem` 4 == 0 && year `rem` 100 /= 0 || year `rem` 400 == 0
