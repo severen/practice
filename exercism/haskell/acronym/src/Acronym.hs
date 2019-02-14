b27b527a4677ec5e641941caa82ae00914c0d966
Severen Redwood <severen@shrike.me>
2019-02-15 11:49:27 +1300

    Exercism (Haskell): Add solution for "Acronym"

---
+++ exercism/haskell/acronym/src/Acronym.hs
@@ -0,0 +1,22 @@
+module Acronym (abbreviate) where
+
+import Data.Char (isAlpha, isUpper, toUpper)
+
+-- | Remove apostrophies and replace special characters with whitespace.
+clean :: String -> String
+clean = map (\c -> if isAlpha c then c else ' ') . filter (/= '\'')
+
+-- | Uppercase the first character of a given string.
+capitalize :: String -> String
+capitalize (x:xs) = toUpper x : xs
+capitalize "" = ""
+
+-- | Trim an all caps string down to its first character.
+trimAllCaps :: String -> String
+trimAllCaps string
+  | length string == length (filter isUpper string) = take 1 string
+  | otherwise = string
+
+-- | Abbreviate a given word.
+abbreviate :: String -> String
+abbreviate = filter isUpper . concatMap (trimAllCaps . capitalize) . words . clean
