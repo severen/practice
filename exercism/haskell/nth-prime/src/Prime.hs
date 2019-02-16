a3dc622ddb0d30d029fe253a0fb1997250537b08
Severen Redwood <severen@shrike.me>
2019-02-16 22:18:48 +1300

    Exercism (Haskell): Improve readability of "Nth Prime"

--- exercism/haskell/nth-prime/src/Prime.hs
+++ exercism/haskell/nth-prime/src/Prime.hs
@@ -6,7 +6,8 @@ isqrt = floor . (sqrt :: Double -> Double) . fromIntegral
 isPrime :: Int -> Bool
 isPrime n
   | n < 2 = False
-  | otherwise = all (\m -> n `mod` m /= 0) [2..(isqrt n)]
+  | otherwise = all (not . divisibleBy) [2..isqrt n]
+  where divisibleBy m = n `rem` m == 0
 
 nth :: Int -> Maybe Integer
 nth 0 = Nothing
