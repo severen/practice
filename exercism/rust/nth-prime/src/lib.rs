9211aaaac1c61be442d5d7a0f91aae0dd3ee48b5
Severen Redwood <severen@shrike.me>
2019-02-16 22:06:16 +1300

    Exercism (Rust): Clean up solution for "Nth Prime"

--- exercism/rust/nth-prime/src/lib.rs
+++ exercism/rust/nth-prime/src/lib.rs
@@ -1,10 +1,15 @@
+fn isqrt(n: u32) -> u32 {
+    (n as f64).sqrt() as u32
+}
+
 fn is_prime(n: u32) -> bool {
-   // We add 1 to compensate for the conversion from f64 to u32, which truncates
-   // the decimal point and therefore makes upper_bound^2 < n.
-   let upper_bound = (n as f64).sqrt() as u32 + 1;
-   (2..upper_bound).all(|m| n % m != 0)
+    if n < 2 {
+        false
+    } else {
+        (2..=isqrt(n)).all(|m| n % m != 0)
+    }
 }
 
 pub fn nth(n: u32) -> u32 {
-    (1..).filter(|x| is_prime(*x)).nth(n as usize + 1).unwrap()
+    (2..).filter(|x| is_prime(*x)).nth(n as usize).unwrap()
 }
