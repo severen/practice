0a98257b11c90b5a1099b6ea572207e96592d194
Severen Redwood <severen@shrike.me>
2019-02-21 11:44:14 +1300

    Exercism (Haskell): Add solution for "Anagram"

---
+++ exercism/haskell/anagram/src/Anagram.hs
@@ -0,0 +1,14 @@
+module Anagram (anagramsFor) where
+
+import Data.List (sort)
+import Data.Char (toLower)
+
+-- | Check whether string a is an anagram of string b.
+isAnagram :: String -> String -> Bool
+isAnagram a b = a' /= b' && sort a' == sort b'
+  where
+    a' = map toLower a
+    b' = map toLower b
+
+anagramsFor :: String -> [String] -> [String]
+anagramsFor xs xss = filter (isAnagram xs) xss
