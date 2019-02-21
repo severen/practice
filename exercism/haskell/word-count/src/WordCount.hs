80ab16e0d4fed34e0786811ec89861c2ae686ec6
Severen Redwood <severen@shrike.me>
2019-02-21 20:31:58 +1300

    Exercism (Haskell): Add solution for "Word Count"

---
+++ exercism/haskell/word-count/src/WordCount.hs
@@ -0,0 +1,22 @@
+module WordCount (wordCount) where
+
+import Data.Map (Map)
+import Data.Char (toLower, isAlphaNum)
+import Data.List.Split
+
+import qualified Data.Map.Strict as Map
+
+isSeparator :: Char -> Bool
+isSeparator c = c /= '\'' && (not . isAlphaNum) c
+
+unquote :: String -> String
+unquote ('\'':s)
+  | last s == '\'' = init s
+  | otherwise = s
+unquote s = s
+
+wordCount :: String -> Map String Int
+wordCount = Map.fromListWith (+) . freqList . wordList
+  where
+    freqList = map (\s -> (map toLower s, 1))
+    wordList = map unquote . wordsBy (isSeparator)
