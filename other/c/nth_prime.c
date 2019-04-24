30be5ac9a81e0dbdee63c3423279b88a643e0360
Severen Redwood <severen@shrike.me>
2019-04-24 17:36:35 +1200

    Other (C): Add implementation of "Nth Prime" in C

---
+++ other/c/nth_prime.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+int isqrt(int n) {
+  return (int)floor(sqrt(n));
+}
+
+bool is_prime(int n) {
+  if (n < 2) { return false; }
+
+  for (int m = 2; m <= isqrt(n); m++) {
+    if (n % m == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int nth_prime(unsigned int n) {
+  int counter = 0;
+
+  if (n == 0) { exit(EXIT_FAILURE); }
+
+  for (int i = 2; ; i++) {
+    if (is_prime(i)) {
+      counter++;
+      
+      if (counter == n) {
+        return i;
+      }
+    }
+  }
+}
+
+int main() {  
+  for (int i = 1; i <= 10; i++) {
+    printf("Prime #%d: %d\n", i, nth_prime(i));
+  }
+
+  return 0;
+}
