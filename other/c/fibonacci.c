d51706c9b4ff4a97f431f806322a90a976f7ed65
Severen Redwood <severen@shrike.me>
2019-08-10 19:22:39 +1200

    Other (C): Add implementation of the Fibonacci sequence

---
+++ other/c/fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int fibonacci(int n) {
+  if (n < 0) { exit(EXIT_FAILURE); }
+  
+  double phi = (1 + sqrt(5)) / 2;
+  double psi = 1 - phi;
+  
+  // Binet's Formula
+  return (int)((pow(phi, n) - pow(psi, n)) / sqrt(5));
+}
+
+int main() {
+  for (int i = 0; i < 10; i++) {
+    printf("Fibonacci number #%d: %d\n", i + 1, fibonacci(i));
+  }
+
+  return 0;
+}
