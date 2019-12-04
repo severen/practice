9e6d8f51820a76b77155b26c17dd1d10e618cc67
Severen Redwood <severen@shrike.me>
2019-12-04 20:43:07 +1300

    AoC (Day 1): Add solution for part 2

--- advent-of-code/2019/rust/day01/src/main.rs
+++ advent-of-code/2019/rust/day01/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, iter};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -6,6 +6,7 @@ fn main() -> Result<()> {
     let input = fs::read_to_string("input.txt")?;
 
     part1(&input)?;
+    part2(&input)?;
 
     Ok(())
 }
@@ -15,6 +16,29 @@ fn part1(input: &str) -> Result<()> {
         .lines()
         .map(|line| line.parse::<i32>().unwrap())
         .map(|mass| (mass / 3) - 2)
+        .sum();
+
+    println!("{}", required_fuel);
+
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    let calc = |mass: i32| ((mass / 3) - 2).max(0);
+
+    let required_fuel: i32 = input
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .map(calc)
+        .flat_map(|mass| {
+            iter::successors(Some(mass), |&x| {
+                if x <= 0 {
+                    None
+                } else {
+                    Some(calc(x))
+                }
+            })
+        })
         .sum();
 
     println!("{}", required_fuel);
