902cede2a6431fb6d498a9872b78ffa477094581
Severen Redwood <severen@shrike.me>
2019-12-12 19:17:31 +1300

    AoC (Day 4): Only break up each number into its digits once

--- advent-of-code/2019/rust/day04/src/main.rs
+++ advent-of-code/2019/rust/day04/src/main.rs
@@ -2,6 +2,9 @@ use std::fs;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+/// A 6-digit number broken up into an array of its digits.
+type Number = [i32; 6];
+
 fn main() -> Result<()> {
     let input = fs::read_to_string("input.txt")?;
     let range: Vec<i32> = input
@@ -17,17 +20,18 @@ fn main() -> Result<()> {
 
 fn part1(range: &Vec<i32>) {
     let possibilities = (range[0]..=range[1])
+        .map(digits)
         .filter(has_adjacent_digits)
         .filter(has_nondecreasing_digits)
-        .collect::<Vec<i32>>()
+        .collect::<Vec<Number>>()
         .len();
 
     println!("Part 1: {}", possibilities);
 }
 
 /// Convert a 6-digit number into an array of its digits.
-fn digits(x: i32) -> [i32; 6] {
-    let mut digits: [i32; 6] = [0; 6];
+fn digits(x: i32) -> Number {
+    let mut digits: Number = [0; 6];
     let mut x = x;
 
     for i in (0..6).rev() {
@@ -38,9 +42,9 @@ fn digits(x: i32) -> [i32; 6] {
     digits
 }
 
-/// Determine if a number has adjacent digits.
-fn has_adjacent_digits(x: &i32) -> bool {
-    for window in digits(*x).windows(2) {
+/// Determine if a 6-digit number has adjacent digits.
+fn has_adjacent_digits(x: &Number) -> bool {
+    for window in x.windows(2) {
         if window[0] == window[1] {
             return true
         }
@@ -49,9 +53,9 @@ fn has_adjacent_digits(x: &i32) -> bool {
     false
 }
 
-/// Determine if a number has nondecreasing digits.
-fn has_nondecreasing_digits(x: &i32) -> bool {
-    for window in digits(*x).windows(2) {
+/// Determine if a 6-digit number has nondecreasing digits.
+fn has_nondecreasing_digits(x: &Number) -> bool {
+    for window in x.windows(2) {
         if window[1] < window[0] {
             return false
         }
