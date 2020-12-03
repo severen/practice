9336b42d17bb42ef25afaf44b0b4dcfab166dbcc
Severen Redwood <severen@shrike.me>
2020-12-04 03:19:31 +1300

    AoC (Day 1): Add C++ solutions for part 1 and part 2

---
+++ advent-of-code/2020/cpp/day01.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+
+std::vector<int> parse_input(std::ifstream &input) {
+  std::vector<int> xs;
+
+  std::string line;
+  while (getline(input, line)) {
+    xs.push_back(std::stoi(line));
+  }
+
+  return xs;
+}
+
+void part1(std::vector<int> &xs, int target) {
+  std::set<int> seen;
+
+  for (auto x : xs) {
+    auto y = target - x;
+
+    // TODO: Use C++20's set::contains method instead.
+    if (seen.find(y) != seen.end()) {
+      std::cout << "Part 1: " << x * y << "\n";
+      return;
+    }
+
+    seen.insert(x);
+  }
+}
+
+void part2(std::vector<int> &xs, int target) {
+  for (std::size_t i = 0; i < xs.size(); i++) {
+    std::set<int> seen;
+
+    for (std::size_t j = i + 1; j < xs.size(); j++) {
+      auto x = xs[i];
+      auto y = xs[j];
+      auto z = target - x - y;
+
+      // TODO: Use C++20's set::contains method instead.
+      if (seen.find(z) != seen.end()) {
+        std::cout << "Part 2: " << x * y * z << "\n";
+        return;
+      }
+
+      seen.insert(y);
+    }
+  }
+}
+
+int main() {
+  int target = 2020;
+
+  std::ifstream input("day01.txt");
+  auto xs = parse_input(input);
+
+  part1(xs, target);
+  part2(xs, target);
+
+  return 0;
+}
