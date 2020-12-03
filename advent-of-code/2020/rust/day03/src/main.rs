478bb42b20fd8effa54fe59ac830509341ed48c6
Severen Redwood <severen@shrike.me>
2020-12-03 20:24:26 +1300

    AoC (Day 3): Add solution for part 2

--- advent-of-code/2020/rust/day03/src/main.rs
+++ advent-of-code/2020/rust/day03/src/main.rs
@@ -12,23 +12,38 @@ fn main() -> Result<()> {
     .collect();
 
   part1(&map);
+  part2(&map);
 
   Ok(())
 }
 
 fn part1(map: &Map) {
-  let mut count = 0;
+  println!("Part 1: {}", count_trees(&map, (1, 3)));
+}
 
+fn part2(map: &Map) {
+  let slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)];
+  let result = slopes
+    .iter()
+    .fold(1, |acc, slope| acc * count_trees(map, *slope));
+
+  println!("Part 2: {}", result);
+}
+
+fn count_trees(map: &Map, slope: (usize, usize)) -> usize {
   let rows = map.len();
   let cols = map[0].len();
+  let (dy, dx) = slope;
 
-  for i in 0..rows {
-    let square = map[i][(i * 3) % cols];
+  let mut count = 0;
+
+  for i in 0..rows / dy {
+    let square = map[i * dy][(i * dx) % cols];
 
     if square == '#' {
       count += 1;
     }
   }
 
-  println!("Part 1: {}", count);
+  count
 }
