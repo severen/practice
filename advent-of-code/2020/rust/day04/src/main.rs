a666a4731b8906d6cd3b766a302be8d08f13ad10
Severen Redwood <severen@shrike.me>
2020-12-08 21:51:23 +1300

    AoC (Day 4): Improve solution for part 1

--- advent-of-code/2020/rust/day04/src/main.rs
+++ advent-of-code/2020/rust/day04/src/main.rs
@@ -1,15 +1,18 @@
-use std::{result, error, fs, collections::HashSet};
+use std::{result, error, fs, collections::HashMap};
 
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
-const REQUIRED_FIELDS: [&'static str; 7] =
+type Passport<'a> = HashMap<&'a str, &'a str>;
+
+const REQUIRED_KEYS: [&'static str; 7] =
   ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
 fn main() -> Result<()> {
   let input = fs::read_to_string("input.txt")?;
-  let passports: Vec<String> = input
+  let passports: Vec<Passport> = input
+    // TODO: Figure out a platform-agnostic way to split on an empty line.
     .split("\n\n")
-    .map(|line| line.replace('\n', " "))
+    .map(parse_passport)
     .collect();
 
   part1(&passports);
@@ -17,11 +20,11 @@ fn main() -> Result<()> {
   Ok(())
 }
 
-fn part1(passports: &[String]) {
+fn part1(passports: &[Passport]) {
   let mut count = 0;
 
   for passport in passports {
-    if is_valid(passport) {
+    if has_required_keys(passport) {
       count += 1;
     }
   }
@@ -29,14 +32,19 @@ fn part1(passports: &[String]) {
   println!("Part 1: {}", count);
 }
 
-fn is_valid(passport: &str) -> bool {
-  let present_fields: HashSet<&str> = passport
+fn parse_passport(text: &str) -> Passport {
+  let fields = text
     .split_whitespace()
-    .map(|f| f.split(':').nth(0).unwrap())
-    .collect();
+    .flat_map(|f| f.split(':'));
+  let keys = fields.clone().step_by(2);
+  let values = fields.skip(1).step_by(2);
 
-  for field in &REQUIRED_FIELDS {
-    if !present_fields.contains(field) {
+  keys.zip(values).collect()
+}
+
+fn has_required_keys(passport: &Passport) -> bool {
+  for key in &REQUIRED_KEYS {
+    if !passport.contains_key(key) {
       return false;
     }
   }
