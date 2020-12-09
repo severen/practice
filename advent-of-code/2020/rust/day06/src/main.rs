91c1962b9eecefc9ee11e53f70cb1bcf93a2e03e
Severen Redwood <severen@shrike.me>
2020-12-10 11:28:32 +1300

    AoC (Day 6): Add Rust solution for part 1 and part 2

---
+++ advent-of-code/2020/rust/day06/src/main.rs
@@ -0,0 +1,49 @@
+#![feature(iterator_fold_self)]
+
+use std::{result, error, fs, collections::HashSet};
+
+type Result<T> = result::Result<T, Box<dyn error::Error>>;
+
+fn main() -> Result<()> {
+  let input = fs::read_to_string("input.txt")?;
+  let groups: Vec<_> = input
+    .split_terminator("\n\n")
+    .collect();
+
+  part1(&groups);
+  part2(&groups);
+
+  Ok(())
+}
+
+fn part1(groups: &[&str]) {
+  let count: usize = groups
+    .iter()
+    .map(|group| parse_group_v1(*group))
+    .map(|answers| answers.len())
+    .sum();
+
+  println!("Part 1: {}", count);
+}
+
+fn part2(groups: &[&str]) {
+  let count: usize = groups
+    .iter()
+    .map(|group| parse_group_v2(*group))
+    .map(|answers| answers.len())
+    .sum();
+
+  println!("Part 2: {}", count);
+}
+
+fn parse_group_v1(group: &str) -> HashSet<char> {
+  group.lines().flat_map(|line| line.chars()).collect()
+}
+
+fn parse_group_v2(group: &str) -> HashSet<char> {
+  group
+    .lines()
+    .map(|line| line.chars().collect::<HashSet<_>>())
+    .fold_first(|a, b| &a & &b)
+    .unwrap()
+}
