ac30c98463ebd6e4727fc87a6d137745aebba6c2
Severen Redwood <severen@shrike.me>
2020-12-18 01:36:26 +1300

    Other (C++): Add isogram checking algorithm

---
+++ other/cpp/isogram.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <cctype>
+
+bool is_isogram(const std::string& word) {
+  std::array<char, 26> found = { 0 };
+
+  for (auto c : word) {
+    if (!isalpha(c)) {
+      continue;
+    }
+
+    size_t i = (size_t)tolower(c) - 'a';
+    if (!found[i]) {
+      found[i] = true;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Please provide a word.\n";
+    return 1;
+  }
+
+  std::string word = argv[1];
+  if (is_isogram(word)) {
+    std::cout << "The given word is an isogram.\n";
+  } else {
+    std::cout << "The given word is not an isogram.\n";
+  }
+
+  return 0;
+}
