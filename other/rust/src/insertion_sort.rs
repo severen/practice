32f1354da20966d978a1c6f0ef1ec58788aea2d3
Severen Redwood <severen@shrike.me>
2021-06-24 21:46:33 +1200

    Other (Rust): Fix indentation

--- other/rust/src/insertion_sort.rs
+++ other/rust/src/insertion_sort.rs
@@ -1,11 +1,11 @@
 pub fn insertion_sort(xs: &mut [impl Ord]) {
-    for i in 0..xs.len() {
-        for j in (0..i).rev() {
-            if xs[j] > xs[j + 1] {
-                xs.swap(j, j + 1);
-            } else {
-                break;
-            }
-        }
+  for i in 0..xs.len() {
+    for j in (0..i).rev() {
+      if xs[j] > xs[j + 1] {
+        xs.swap(j, j + 1);
+      } else {
+        break;
+      }
     }
+  }
 }
--- other/rust/src/main.rs
+++ other/rust/src/main.rs
@@ -3,7 +3,7 @@ mod insertion_sort;
 use crate::insertion_sort::insertion_sort;
 
 fn main() {
-    let mut nums = [2, 1, 3, 5, 6, 7, 4];
-    insertion_sort(&mut nums);
-    dbg!(&nums);
+  let mut nums = [2, 1, 3, 5, 6, 7, 4];
+  insertion_sort(&mut nums);
+  dbg!(&nums);
 }
