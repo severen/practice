1a43a191e50d0806994adbf841471478c974ceff
Severen Redwood <severen@shrike.me>
2021-06-25 21:58:26 +1200

    Exercism (Rust): Improve solution for "Nth Prime"

--- exercism/rust/nth-prime/src/lib.rs
+++ exercism/rust/nth-prime/src/lib.rs
@@ -1,15 +1,19 @@
 fn isqrt(n: u32) -> u32 {
-    (n as f64).sqrt() as u32
+  (n as f64).sqrt() as u32
 }
 
 fn is_prime(n: u32) -> bool {
-    if n < 2 {
-        false
-    } else {
-        (2..=isqrt(n)).all(|m| n % m != 0)
-    }
+  if n < 2 || n > 2 && n % 2 == 0 {
+    false
+  } else if n == 2 {
+    true
+  } else {
+    // We take advantage of the fact that the only prime numbers with a gap less
+    // than 2 are 2 and 3, which are already covered conditionally.
+    (3..=isqrt(n)).step_by(2).all(|m| n % m != 0)
+  }
 }
 
 pub fn nth(n: u32) -> u32 {
-    (2..).filter(|x| is_prime(*x)).nth(n as usize).unwrap()
+  (2..).filter(|x| is_prime(*x)).nth(n as usize).unwrap()
 }
