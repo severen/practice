bfc58a9a372ab535183bcf53918b32e95b913824
Severen Redwood <severen@shrike.me>
2021-07-15 05:36:00 +1200

    Other (Racket): Use even number optimisation in nth_prime

--- other/racket/primes.rkt
+++ other/racket/primes.rkt
@@ -7,10 +7,14 @@
   (define (go n c)
     (cond [(> c (integer-sqrt n)) #t]
           [(zero? (modulo n c)) #f]
-          [else (go n (add1 c))]))
-  (if (< n 2)
-      #f
-      (go n 2)))
+          ;; We take advantage of the fact that the only prime numbers with a
+          ;; gap less than 2 are 2 and 3, which are already covered
+          ;; conditionally.
+          [else (go n (+ c 2))]))
+  (cond [(< n 2) #f]
+        [(= n 2) #t]
+        [(and (> n 2) (zero? (modulo n 2))) #f]
+        [else (go n 3)]))
 
 ;; Find the next prime number after n, where n is a natural number.
 (define (next-prime n)
