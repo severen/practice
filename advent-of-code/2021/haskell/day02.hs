c9f37d7c015ff903173acb0fd6bbc4c9fc1470c2
Severen Redwood <severen@shrike.me>
2021-12-04 19:48:41 +1300

    AoC (Day 2): Add solution for part 2

--- advent-of-code/2021/haskell/day02.hs
+++ advent-of-code/2021/haskell/day02.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Data.Char (toUpper)
 
+type Aim = Int
 type Position = (Int, Int)
 data Command = Forward Int | Up Int | Down Int deriving (Read)
 
@@ -11,6 +12,7 @@ main = do
   let cmds = map parseLine input
 
   putStrLn $ "Part 1: " ++ (show $ part1 cmds)
+  putStrLn $ "Part 2: " ++ (show $ part2 cmds)
 
 parseLine :: String -> Command
 parseLine = read . capitalize
@@ -25,3 +27,11 @@ part1 cmds = let (x, y) = foldl executeCommand (0, 0) cmds in x * y
   executeCommand (x, y) (Forward k) = (x + k, y)
   executeCommand (x, y) (Up k) = (x, y - k)
   executeCommand (x, y) (Down k) = (x, y + k)
+
+part2 :: [Command] -> Int
+part2 cmds = let (_, (x, y)) = foldl executeCommand (0, (0, 0)) cmds in x * y
+ where
+  executeCommand :: (Aim, Position) -> Command -> (Aim, Position)
+  executeCommand (aim, (x, y)) (Forward k) = (aim, (x + k, y + k * aim))
+  executeCommand (aim, (x, y)) (Up k) = (aim - k, (x, y))
+  executeCommand (aim, (x, y)) (Down k) = (aim + k, (x, y))
